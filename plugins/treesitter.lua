881b4572775708816bf86380fa2092f6ed4cbf3c
Nate Kidwell <ludicast@users.noreply.github.com>
2023-04-20 07:06:27 -0400

    ugh

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,6 +1,6 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
-    ensure_installed = { "lua", "rust", "typescrypt", "c", "ruby", "scala", "go", "python" },
+    ensure_installed = { "lua", "rust", "typescript", "c", "ruby", "scala", "go", "python" },
   },
 }
